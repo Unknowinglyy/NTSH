d2915136f0dc29de0b2510b81ab5e17729ed49c1
8davxgnu <davxgnu@tamu.edu>
2024-09-20 04:36:14 -0500

    UPDATE oscilloscope2.py

--- lab3/dc_test/oscilloscope2.py
+++ lab3/dc_test/oscilloscope2.py
@@ -53,7 +53,7 @@ def measure_voltage(sample_rate=10):
             mode_count = mode_change.count[0]
             print(f"Mode of Changes: {mode_value:.2f} V, Count: {mode_count}")
 
-            if average_change <= 0.02:
+            if average_change == 0:
                 print("NO WAVE")
             if (len(np.unique(voltageChangeArr)) <= 3) and average_change > 0:
                 print("SQUARE WAVE")
