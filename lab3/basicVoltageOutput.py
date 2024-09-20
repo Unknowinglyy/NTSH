aa3192ae61d50121fe58eaf1be63e28b62cc64a7
8davxgnu <davxgnu@tamu.edu>
2024-09-20 02:52:06 -0500

    UPDATE test4

--- lab3/basicVoltageOutput.py
+++ lab3/basicVoltageOutput.py
@@ -21,7 +21,7 @@ def measure_voltage(sample_rate=10):
     count = 0  # Initialize count for readings
     while True:
         voltage = chan0.voltage
-        print(f"Voltage: {voltage:.4f} V")
+        print(f"Voltage: {voltage:.1f} V")
         
         count += 1
         if count % 10 == 0:  # Every 10 readings
