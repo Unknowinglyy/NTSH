b1be4657bc6e137a52cb9c89096b4b363302b59d
8davxgnu <davxgnu@tamu.edu>
2024-10-02 14:10:24 -0500

    ADD touchScreen...

--- Ballance/touchScreenBasicCoordOutput.py
+++ Ballance/touchScreenBasicCoordOutput.py
@@ -22,4 +22,4 @@ for event in device.read_loop():
             print(f"Touch at X: {x}, Y: {y}")
             x, y = None, None  # Reset after output
     elif event.type == evdev.ecodes.EV_KEY:
-            
+            print("No touch")
