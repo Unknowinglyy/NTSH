8697736873b32e2d117b5b456c3af9e69bc78d2c
Blake Dejohn <unknowingly@tamu.edu>
2024-10-02 13:05:34 -0500

    typo

--- Ballance/test.py
+++ Ballance/test.py
@@ -8,13 +8,13 @@ print(f"Listening for events from {device_path}...")
 
 try:
     for event in device.read_loop():
-        if event.type == evdev.encodes.EV_ABS:
+        if event.type == evdev.ecodes.EV_ABS:
             absevent = evdev.categorize(event)
             print(f"Absolute event: code = {absevent.event.code}, value = {absevent.event.value}")
-        elif event.type == evdev.encodes.EV_KEY:
+        elif event.type == evdev.ecodes.EV_KEY:
             keyevent = evdev.categorize(event)
             print(f"Key event: code = {keyevent.event.code}, value = {keyevent.event.value}")
-        elif event.type == evdev.encodes.EV_SYN:
+        elif event.type == evdev.ecodes.EV_SYN:
             print("Sync event")
 except KeyboardInterrupt:
     print("Exiting...")
