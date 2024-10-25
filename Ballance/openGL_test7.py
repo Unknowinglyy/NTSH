5a139ae30ea882c3271ddc40dbea4e4a8ecc1c39
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-10-25 16:29:37 -0500

    testing rotate?

--- Ballance/openGL_test7.py
+++ Ballance/openGL_test7.py
@@ -157,6 +157,12 @@ def main():
 
     clock = pygame.time.Clock()
 
+    glTranslatef(0.0, 0.0, -5.0)  
+    glRotatef(90, 1.0, 0.0, 0.0) # hard coded pitch tilt
+    glRotatef(10, -1.0, 0.0, 0.0) # hard coded pitch tilt
+    glRotatef(10, 0.0, 0.0, -1.0) # hard coded roll tilt
+    glRotatef(3, 0.0, 1.0, 0.0)
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -182,12 +188,6 @@ def main():
         # gluLookAt(0, 0.5, 5,  # Camera position
         #           prism_top_center[0], prism_top_center[1], prism_top_center[2],  # Look at point
         #           0, 1, 0)  # Up direction
-
-        glTranslatef(0.0, 0.0, -5.0)  
-        glRotatef(90, 1.0, 0.0, 0.0) # hard coded pitch tilt
-        glRotatef(10, -1.0, 0.0, 0.0) # hard coded pitch tilt
-        glRotatef(10, 0.0, 0.0, -1.0) # hard coded roll tilt
-        glRotatef(3, 0.0, 1.0, 0.0)
         
 
         glRotatef(pitch, 1, 0.0, 0.0) # up down
