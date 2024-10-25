0ac3e1907d554550cd6f4924678914d970997157
Blake Dejohn <unknowingly@tamu.edu>
2024-10-25 15:06:44 -0500

    negate that z

--- Ballance/openGL_test5.py
+++ Ballance/openGL_test5.py
@@ -115,7 +115,7 @@ def update_points():
         print("converting to gl coordinates...")
         gl_x = ((y-150) / (3940 - 150)) * 0.4 - 0.2
         gl_y = 0.3
-        gl_z = ((x-250) / (3800 - 250)) * 2 - 1
+        gl_z = -(((x-250) / (3800 - 250)) * 2 - 1)
         print(f"corresponding gl coordinates: {gl_x}, {gl_y}, {gl_z}")
         #if the length of the points list is 100, remove the oldest point
         #else just append the new point
