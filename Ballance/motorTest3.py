aaebf8723b8a5c9975a70e507e66b5ea39bdf2ef
Blake Dejohn <unknowingly@tamu.edu>
2024-11-06 14:18:01 -0600

    running the motor faster

--- Ballance/motorTest3.py
+++ Ballance/motorTest3.py
@@ -7,7 +7,7 @@ dir_pin = 21   # Pin connected to DIR on TMC2208
 
 # Motor movement parameters
 test_steps = 200              # Number of steps to move in each direction
-delay_time = 0.005          # Delay in seconds between steps
+delay_time = 0.001          # Delay in seconds between steps
 
 # Setup GPIO
 step = OutputDevice(step_pin)
