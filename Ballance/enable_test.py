b3c8164352e36b74edb5d10a68ccf86ae2ed4f83
Blake Dejohn <unknowingly@tamu.edu>
2024-11-11 13:30:22 -0600

    enable test

---
+++ Ballance/enable_test.py
@@ -0,0 +1,21 @@
+from gpiozero import OutputDevice
+import time
+
+enable_pin = 4 # Pin connected to EN on TMC2208
+
+# Motor movement parameters
+test_steps = 200              # Number of steps to move in each direction
+delay_time = 0.005          # Delay in seconds between steps
+
+enable = OutputDevice(enable_pin, initial_value=False)
+
+try:
+    print("Starting enable test...")
+    enable.on()
+
+except KeyboardInterrupt:
+    print("enable test interrupted.")
+
+finally:
+    enable.off()
+    print("exiting.")
