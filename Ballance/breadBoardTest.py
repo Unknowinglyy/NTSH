05b2628a4c78e75255ad52a3a77dc9e1749a6193
Blake Dejohn <unknowingly@tamu.edu>
2024-11-11 13:43:35 -0600

    organizing

--- Ballance/breadBoardTest.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import RGBLED, Button, LEDCharDisplay
-from time import sleep  
-import threading
-    
-while True:
-    print("Test Message")
-    sleep(7)
--- Ballance/openGL/openGL_test7.py
+++ Ballance/openGL/openGL_test7.py
@@ -9,7 +9,8 @@ import busio
 import adafruit_mpu6050
 import math
 import time
-from touchScreenBasicCoordOutput import read_touch_coordinates
+from touchscreen.touchScreenBasicCoordOutput import read_touch_coordinates
+
 
 # Initialize I2C and MPU6050
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -42,11 +43,13 @@ def init():
     glShadeModel(GL_SMOOTH)
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glClearDepth(1.0)
-    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_DEPTH_TEST, GL_BLEND)
     glDepthFunc(GL_LEQUAL)
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
     glEnable(GL_POINT_SMOOTH)
     glPointSize(5.0)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
 
 def draw_rect():
 
@@ -92,6 +95,8 @@ def draw_rect():
 
 def draw_points():
     current_time = time.time()
+    glEnable(GL_POINT_SMOOTH)  
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
     glColor3f(0.0, 0.0, 1.0)
     glBegin(GL_POINTS)
     for point, timestamp in points:
@@ -116,7 +121,7 @@ def update_points():
         print(f"reading touch coordinates: {x}, {y}")
         print("converting to gl coordinates...")
         gl_x = ((y-150) / (3940 - 150)) * 2 - 1
-        gl_y = 0.3  
+        gl_y = 0.3
         gl_z = -(((x-250) / (3800 - 250)) * 2 - 1)
         print(f"corresponding gl coordinates: {gl_x}, {gl_y}, {gl_z}")
         #if the length of the points list is 100, remove the oldest point
@@ -151,11 +156,10 @@ def get_orientation(dt):
     roll = alpha * roll + (1 - alpha) * accel_roll
 
 def draw_text(x, y, text):
-    text_surface = font.render(text, True, (255, 255, 255, 255), (0, 66, 0, 255))
+    text_surface = font.render(text, True, (255, 255, 255, 255), (0, 0, 0, 255))
     text_data = pygame.image.tostring(text_surface, "RGBA", True)
     glWindowPos2d(x, y)
     glDrawPixels(text_surface.get_width(), text_surface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, text_data)
-
 
 def main():
     resize(800, 600)
@@ -177,7 +181,7 @@ def main():
             #     display()
         
         #display()
-        time.sleep(0.01/2)
+        time.sleep(0.01)
         
 
         dt = clock.tick(60) / 1000.0
@@ -191,13 +195,19 @@ def main():
         # gluLookAt(0, 0.5, 5,  # Camera position
         #           prism_top_center[0], prism_top_center[1], prism_top_center[2],  # Look at point
         #           0, 1, 0)  # Up direction
-        glTranslatef(0, 0, -5.0)
-        glRotatef(pitch, 1, 0.0, 0.0)
-        glRotatef(yaw, 0.0, -1, 0.0)
-        glRotatef(roll, 0.0, 0.0, -1)
+
+        glTranslatef(0.0, 0.0, -5.0)  
+        glRotatef(90, 1.0, 0.0, 0.0) # hard coded pitch tilt
+        glRotatef(10, -1.0, 0.0, 0.0) # hard coded pitch tilt
+        glRotatef(10, 0.0, 0.0, -1.0) # hard coded roll tilt
+        glRotatef(3, 0.0, 1.0, 0.0)
+        
+
+        glRotatef(pitch, 1, 0.0, 0.0) # up down
+        glRotatef(yaw, 0.0, -1, 0.0) # side to side
+        glRotatef(roll, 0.0, 0.0, -1) # tilt
         draw_rect()
         draw_points()
-
         draw_text(-0.95, 0.9, f"Current Position: {current_position[0]}, {current_position[1]}")
         pygame.display.flip()
         #clock.tick(60)
