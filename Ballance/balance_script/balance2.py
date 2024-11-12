c421f29b1f1d0ade6f812b7a10c913c66d899a16
Blake Dejohn <unknowingly@tamu.edu>
2024-11-11 18:00:02 -0600

    change dir3

--- Ballance/balance_script/balance2.py
+++ Ballance/balance_script/balance2.py
@@ -72,9 +72,9 @@ if __name__ == "__main__":
                 move_motor(step, -error_y, direction, True)
 
             if error_x < 0 and error_y < 0:
-                move_motor(step3, -error_x, direction3, False)
+                move_motor(step3, -error_x, direction3, True)
             elif error_x > 0 and error_y > 0:
-                move_motor(step3, error_x, direction3, True)
+                move_motor(step3, error_x, direction3, False)
 
             print(f"X: {x}, Y: {y}, Error X: {error_x}, Error Y: {error_y}")
 
