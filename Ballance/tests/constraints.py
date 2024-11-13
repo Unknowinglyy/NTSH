88174919b3a70b097e1934d7fb76290baa222106
Blake Dejohn <unknowingly@tamu.edu>
2024-11-13 14:26:54 -0600

    more sleeps

--- Ballance/tests/constraints.py
+++ Ballance/tests/constraints.py
@@ -54,13 +54,15 @@ if __name__ == "__main__":
         move_motor(step2, test_steps, direction2, True)  # Motor 2 clockwise
         time.sleep(1)
         move_motor(step3, test_steps, direction3, True)  # Motor 3 clockwise
+        time.sleep(1)
 
         move_motor(step, test_steps, direction, True)  # Motor 1 clockwise
         time.sleep(1)
         move_motor(step2, test_steps, direction2, True)  # Motor 2 clockwise
         time.sleep(1)
         move_motor(step3, test_steps, direction3, True)  # Motor 3 clockwise
-
+        time.sleep(1)
+        
         move_motor(step, test_steps, direction, True)  # Motor 1 clockwise
         time.sleep(1)
         move_motor(step2, test_steps, direction2, True)  # Motor 2 clockwise
