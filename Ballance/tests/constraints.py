ed5289db811c5c80662db6f0e3ed1f625e65b30c
Blake Dejohn <unknowingly@tamu.edu>
2024-11-15 14:07:00 -0600

    testing one at a time

--- Ballance/tests/constraints.py
+++ Ballance/tests/constraints.py
@@ -70,11 +70,8 @@ if __name__ == "__main__":
         time.sleep(3)
 
         #testing how much the motors can move from their current position
-        move_motor(step, 100, direction, True)  # Motor 1 clockwise
+        move_motor(step, 200, direction, True)  # Motor 1 clockwise
         time.sleep(1)
-        move_motor(step2, 100, direction2, True)  # Motor 2 clockwise
-        time.sleep(1)
-        move_motor(step3, 100, direction3, True)  # Motor 3 clockwise
 
 
         
