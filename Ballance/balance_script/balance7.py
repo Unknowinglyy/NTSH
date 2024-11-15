cad3ce06658b9a2f3992c993c1784d1957391538
8davxgnu <davxgnu@tamu.edu>
2024-11-15 15:31:39 -0600

    TEST

--- Ballance/balance_script/balance7.py
+++ Ballance/balance_script/balance7.py
@@ -43,9 +43,9 @@ def move_motor(motor, steps, clockwise):
     GPIO.output(MOTOR_PINS[motor]['dir'], GPIO.HIGH if clockwise else GPIO.LOW)
     for _ in range(abs(steps)):
         GPIO.output(MOTOR_PINS[motor]['step'], GPIO.HIGH)
-        time.sleep(0.001)
+        time.sleep(0.005)
         GPIO.output(MOTOR_PINS[motor]['step'], GPIO.LOW)
-        time.sleep(0.001)
+        time.sleep(0.005)
 
 # Main loop
 def balance_ball():
