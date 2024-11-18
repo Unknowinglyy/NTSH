00cc77cdb0d35f189489cb4ef79624e95f2b2c43
8davxgnu <davxgnu@tamu.edu>
2024-11-18 14:11:13 -0600

    ADD new test blanace script

--- Ballance/balance_script/balance9.py
+++ Ballance/balance_script/balance9.py
@@ -31,8 +31,8 @@ pid_y = PID(3, 0.9, 0.05, setpoint=CENTER_Y)
 # Configure sample time (update frequency) and output limits
 pid_x.sample_time = 0.01  # 10 ms update rate
 pid_y.sample_time = 0.01
-pid_x.output_limits = (-10, 10)  # Limit to ±10 steps
-pid_y.output_limits = (-10, 10)
+pid_x.output_limits = (-100, 100)  # Limit to ±10 steps
+pid_y.output_limits = (-100, 100)
 
 # --------------------------------------------------------------------------------------------
 def move_motor(motor, steps, clockwise):
