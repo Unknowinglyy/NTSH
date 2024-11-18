6743431f1d052880356b37f30f2952ff5fe16889
8davxgnu <davxgnu@tamu.edu>
2024-11-18 14:44:17 -0600

    ADD new test blanace script

--- Ballance/balance_script/balance9.py
+++ Ballance/balance_script/balance9.py
@@ -25,8 +25,8 @@ for motor in MOTOR_PINS.values():
     GPIO.setup(motor['dir'], GPIO.OUT)
 
 # PID controllers for X and Y directions
-pid_x = PID(1, 0.1, 0.05, setpoint=CENTER_X)
-pid_y = PID(1, 0.1, 0.05, setpoint=CENTER_Y)
+pid_x = PID(1, 0.1, 1, setpoint=CENTER_X)
+pid_y = PID(1, 0.1, 1, setpoint=CENTER_Y)
 
 # Configure sample time (update frequency) and output limits
 pid_x.sample_time = 0.01  # ms update rate
