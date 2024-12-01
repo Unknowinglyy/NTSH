95377371db02c67258913972c327edd9df9b6762
8davxgnu <davxgnu@tamu.edu>
2024-12-01 13:45:53 -0600

    UPDATE openGL6

--- Ballance/openGL/openGL_test6.py
+++ Ballance/openGL/openGL_test6.py
@@ -202,8 +202,8 @@ def main():
         glTranslatef(0, 0, -5.0)
         # glRotatef(90, 1.0, 0.0, 0.0) # hard coded pitch tilt
         glRotatef(10, -1.0, 0.0, 0.0) # hard coded pitch tilt
-        # glRotatef(10, 0.0, 0.0, -1.0) # hard coded roll tilt
-        # glRotatef(3, 0.0, 1.0, 0.0)
+        glRotatef(10, 0.0, 0.0, -1.0) # hard coded roll tilt
+        glRotatef(3, 0.0, 1.0, 0.0)
 
         glRotatef(pitch, 1, 0.0, 0.0)
         glRotatef(yaw, 0.0, -1, 0.0)
