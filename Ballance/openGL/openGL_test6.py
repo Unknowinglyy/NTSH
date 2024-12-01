54b2fba4703be76d6ffcdef4a6eb5dca26dc28ea
8davxgnu <davxgnu@tamu.edu>
2024-12-01 14:26:38 -0600

    UPDATE openGL6

--- Ballance/openGL/openGL_test6.py
+++ Ballance/openGL/openGL_test6.py
@@ -197,7 +197,7 @@ def main():
         # Set the camera position and orientation
         gluLookAt(0, 0.5, zoom_level,  # Camera position (adjusted by zoom_level)
                   0, 0, 0,    # Look at the origin
-                  -0.1, 5, 5)    # Up direction
+                  -0.1, 1, 1)    # Up direction
 
         glTranslatef(0, 0, -5.0)
         glRotatef(pitch, -1, 0.0, 0.0)
